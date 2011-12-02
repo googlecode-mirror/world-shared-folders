565c7a7b264e226c4733602b7f4ff6c812732308
toyprogrammer@gmail.com <toyprogrammer@gmail.com@e94a20e0-c60b-4c68-9595-48936dcbc9ad>
2011-12-02 16:50:16 +0000

    first release

---
+++ trunk/scripts/infoget-1.0.0.0.1.php
@@ -0,0 +1,16 @@
+﻿
+<?php
+/*this script read meta.info file in each directory in directory hierarchy*/
+require "common_lib-1.0.0.0.1.php";
+require "parser_lib-1.0.0.0.1.php";  
+require "test_lib-1.0.0.0.1.php"; // подключаем библиотеку тестирования
+
+
+$path="mag/meta.info";
+$fdata = open_file ($path, "r");
+
+$str="";
+
+  main_test();
+    echo ("<br>***вот и всё))<br>");
+?>
