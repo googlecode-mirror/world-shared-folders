0a9deb323680a715f14176906e0dc2cd7d580515
toyprogrammer@gmail.com <toyprogrammer@gmail.com@e94a20e0-c60b-4c68-9595-48936dcbc9ad>
2011-12-22 14:05:40 +0000

    added first design version

---
+++ trunk/att2/index.php
@@ -0,0 +1,73 @@
+﻿<html>
+<head>
+<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+<title>  Теория хаоса </title>
+<link rel="stylesheet" type="text/css" href="style.css" media="screen">
+</head>
+ <body>
+
+<div id="page">
+	<div id="base">
+		 <div id="size_wrapper">
+		 
+			   <div id="main_menu">
+					<div id="content">
+                      <div id="size_wrapper">
+					  
+					   <div id="main_menu_element">
+							<a href="index.php" title="Home" rel="home" id="home"><img src="images/common/home2.png" alt="Home" /></a>
+					   </div>
+						
+						<div id="main_menu_element">
+					      <a href="test.php" title="Test" rel="test" id="test"><img src="images/common/test.png" alt="Home" /></a>
+					    </div>
+						
+				
+						</div>
+				
+					
+					</div>					
+			   </div>
+
+				 <div id="head">
+					 <div id="promo1">
+						 Теория хаоса
+					 </div>
+				  </div>
+						  
+						  
+		
+				<div id="left_block">
+					<div id="head">
+						 <div id="promo2">
+							 Архив номеров
+							 <hr>
+						 </div>
+						   <div id="content">
+						      <div id="left_block_content">
+								  тут номера
+								   <hr>
+							   </div> 
+							
+						 </div>
+				  </div> 
+			   </div>
+			   <div id="main_text_area">
+				   		 <div id="content">
+						    <div id="main_block_content">
+							
+			                </div>	
+						 </div>	
+			   </div>
+			   
+		</div>	  
+	 </div>		   
+	  
+
+       
+</div>
+
+</body>
+</html>
+
+
