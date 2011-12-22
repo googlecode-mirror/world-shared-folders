a8505f98f8fcd012c1dce43114df8c44e99f7bd9
toyprogrammer@gmail.com <toyprogrammer@gmail.com@e94a20e0-c60b-4c68-9595-48936dcbc9ad>
2011-12-22 17:30:35 +0000

    try more3

---
+++ trunk/scripts/common_lib.php
@@ -0,0 +1,28 @@
+﻿<?php
+function open_file($path ,$optype) // открыть файл php
+{ 
+	$fdata = fopen ($path, $optype);
+	$result=0;
+	if (!$fdata) 
+	  { 
+		echo ("<b>(!)Error = Невозможно открыть файл</b><br>");
+		exit;
+	  }
+	  else $result= $fdata;
+	  return $result; 
+	   
+}
+function tfile_to_str($path) // считываем текстовый файл в строку.
+{ 
+    $str = "";
+
+	$fdata = open_file($path ,"r");
+	while (!feof($fdata)) 
+  { 
+    $simb = fgetc($fdata); // read file charecter by character
+    $str=$str.$simb;
+
+  } // по окончании этого цикла в переменной $str мы размещаем всё содержимое файла метаинформации.
+  return $str; 
+}
+?>
